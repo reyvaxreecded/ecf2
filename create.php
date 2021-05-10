251b3565a3d458e06dc56202cf4959ebb3ebf5a3
xavier <xavier.deredec@live.fr>
2021-05-10 17:10:41 +0200

    correction of array tags

--- create.php
+++ create.php
@@ -9,16 +9,16 @@ if (isset($postdata) && !empty($postdata)) {
     $contenue_article = $data['contenue_article'];
     $statut_article = $data['statut_article'];
     $id_categorie = $data['id_categorie'];
-    $tag[] = $data['tags'];
+    $tag = $data['tags'];
 
-    try{
+    try {
         $bdd->beginTransaction();
 
-        $stmt= $bdd->prepare('INSERT INTO article(titre_article, contenu_article, statut_article, date_creation_article, date_publication_article, id_categorie) VALUES (:titre, :contenu, :statut, :date_creation, :date_publication, :id_categorie)');
+        $stmt = $bdd->prepare('INSERT INTO article(titre_article, contenu_article, statut_article, date_creation_article, date_publication_article, id_categorie) VALUES (:titre, :contenu, :statut, :date_creation, :date_publication, :id_categorie)');
         $req = $stmt->execute([
-            "titre"=> $titre_article,
-            "contenu"=> $contenue_article,
-            "statut"=> $statut_article,
+            "titre" => $titre_article,
+            "contenu" => $contenue_article,
+            "statut" => $statut_article,
             "date_creation" => date('Y-M-D'),
             "date_publication" => date('Y-M-D'),
             "id_categorie" => $id_categorie
@@ -29,15 +29,19 @@ if (isset($postdata) && !empty($postdata)) {
         $stmt = $bdd->prepare('INSERT INTO possede(id_article, id_tag) VALUES (:id_article, :id_tag)');
 
         foreach ($tag as $id)
-        $req = $stmt->execute([
-            "id_article" => $id_article,
-            "id_tag" => $id
-        ]);
+            $req = $stmt->execute([
+                "id_article" => $id_article,
+                "id_tag" => $id
+            ]);
 
         $bdd->commit();
-    }
-    catch (Exception $e) {
-        $bdd -> rollBack();
+
+        echo json_encode([
+            "success" => true,
+            "msg" => "Article ajouter avec succés"
+        ]);
+    } catch (Exception $e) {
+        $bdd->rollBack();
         echo json_encode([
             "success" => false,
             "msg" => $e->getMessage()
